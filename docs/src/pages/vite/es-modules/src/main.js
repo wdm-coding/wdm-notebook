1b4959eb78b5626c2fec1cb44dd3ed7258ba082d
wdm-coding <wdmcoding0620@163.com>
2025-08-21 17:57:17 +0800

    微调

---
+++ docs/src/pages/vite/es-modules/src/main.js
@@ -0,0 +1,15 @@
+import print from './print.js';
+
+export function main() {
+    console.log('Hello from main.js');
+    print();
+}
+export function fun1() {
+    console.log('Hello from fun1');
+}
+export function fun2() {
+    console.log('Hello from fun2');
+}
+export function fun3() {
+    console.log('Hello from fun3');
+}
