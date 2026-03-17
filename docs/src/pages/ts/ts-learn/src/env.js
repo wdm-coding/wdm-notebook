31ed4a6c5dcbdd7e87056905ac371f409168db77
wdm-coding <wdmcoding0620@163.com>
2026-03-17 11:11:26 +0800

    微调

--- docs/src/pages/ts/ts-learn/src/env.js
+++
@@ -1,6 +0,0 @@
-// require('dotenv').config();
-// console.log('USERNAME===',process.env.USERNAME);
-
-const config = require('config')
-const db = config.get('db')
-console.log('db',db);
--- docs/src/pages/ts/ts-learn/src/genericity.d.ts
+++
@@ -1,6 +0,0 @@
-declare const pushArr: <T>(arr: T[], item: T) => T[];
-declare const stringArr: string[];
-declare const numberArr: number[];
-declare function swapGeneric<T, U>(tuple: [T, U]): [U, T];
-declare const arr: [string, number];
-declare const swapped: [number, string];
